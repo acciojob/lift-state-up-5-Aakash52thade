cee815eac0158ea8be013494fe88183ea7f2814a
akash bawantahde <akashbawanthade02@gmail.com>
2025-11-14 13:07:25 +0530

    main changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,25 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
-import LoginForm from "./LoginForm"
+import LoginForm from "./LoginForm";
 
 const App = () => {
   const [isLoggedIn, setLoggedIn] = useState(false);
 
-   function submitHandler(e) {
-      e.preventDefault();
-      setLoggedIn(true);
+  function handleLogin(e) {
+    e.preventDefault();
+    setLoggedIn(true);
   }
+
   return (
     <div>
-        
-        {
-          !isLoggedIn  ?
-          <LoginForm submitHandler={submitHandler} /> 
-          : 
-                      <p>You are logged in!</p>
-
-        }
-
+      <h1>Parent Component</h1>
+      {!isLoggedIn ? (
+        <LoginForm handleLogin={handleLogin} />
+      ) : (
+        <p>You are logged in!</p>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,30 +1,30 @@
+import React, { useState } from "react";
 
-import React, {useState} from "react";
-export default function LoginForm({submitHandler}){
+export default function LoginForm({ handleLogin }) {
     const [name, setName] = useState("");
-    const [pass, setPass] = useState("")
+    const [pass, setPass] = useState("");
 
-    return(
-        <form  onSubmit={submitHandler}>
-          <label htmlFor="username">Username:</label>
-          <input type="text"
-           id="username"
-           placeholder="username"
-           value={name}
-           onChange={(e) => setName(e.target.value)}
-        />
+    return (
+        <form onSubmit={handleLogin}>
+            <label htmlFor="username">Username:</label>
+            <input 
+                type="text" 
+                id="username"
+                placeholder="username" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
 
-         <label htmlFor="password">Password:</label>
-          <input type="password"
-           id="password"
-           placeholder="password"
-           value={pass}
-           onChange={(e) => setPass(e.target.value)}
-        />
+            <label htmlFor="password">Password:</label>
+            <input 
+                type="password" 
+                id="password"
+                placeholder="password" 
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+            />
 
-         
-                    <button type="submit">Login</button>
-
+            <button type="submit">Login</button>
         </form>
-    )
+    );
 }
